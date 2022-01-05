7101306215f0ba66b909e3251b86f10cffe193c0
Jason Guo <81202082+jguoamz@users.noreply.github.com>
2022-01-05 12:25:30 -0800

    Create file.py

---
+++ file.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+
+# This is Hello World with Python multithreading.
+# A user defined function is created and
+# the function is called when a thread is initialized.
+
+
+import threading
+
+
+def MyFunction():
+    """This is a user defined function"""
+    print "Hello World"
+    fp = open("file.abc")
+    return
+
+
+def Main():
+    """This is where we create a thread.
+    Target means run this function when a thread is initiated."""
+    myThread = threading.Thread(target=MyFunction)
+    myThread.start() 	# Starting a thread
+    fp = open("def.txt")
+    print("dummy operation")
+    print("dummy operation")
+    print("dummy operation")
+    print("dummy operation")
+    fp.close()
+
+
+if __name__ == '__main__':
+    Main()
